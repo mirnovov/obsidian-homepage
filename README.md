ecc6c6baee9f4f62e84f5414c47d43352444d0df
mirnovov <anon185441@gmail.com>
2021-07-25 03:21:25 +0000

    Add workspace functionality Closes #1

--- README.md
+++ README.md
@@ -4,4 +4,7 @@ Opens a specified note upon launching Obsidian, instead of the most recent note.
 
 ## Usage
 
-By default, this plugin opens a note called `Home` in the root directory on startup. This can be changed via the `Open on startup` plugin setting. If the specified note is nonexistent, it will be created.
+By default, this plugin opens a note called `Home` in the root directory on startup. This can be changed in the plugin settings:
+
+* Enable `Use workspaces` to open a workspace of that name instead.
+* Use `Open on startup` to change the note or workspace to open. If a specified note is nonexistent, it will be created.
--- main.ts
+++ main.ts
@@ -1,26 +1,30 @@
-import { App, Plugin, PluginSettingTab, Setting } from "obsidian";
+import { App, Notice, Plugin, PluginSettingTab, Setting } from "obsidian";
 
 interface HomepageSettings {
-	defaultNote: string;
+	defaultNote: string,
+	workspaceEnabled: boolean,
 }
 
-const DEFAULT_SETTINGS: HomepageSettings = {
-	defaultNote: "Home"
+const DEFAULT: HomepageSettings = {
+	defaultNote: "Home",
+	workspaceEnabled: false,
 }
 
 export default class Homepage extends Plugin {
 	settings: HomepageSettings;
+	workspacePlugin: any;
 
 	async onload() {
+		this.workspacePlugin = (<any> this.app)?.internalPlugins?.plugins?.workspaces;
+		
 		await this.loadSettings();
 		this.addSettingTab(new HomepageSettingTab(this.app, this));
 		
 		this.addCommand({
 			id: "open-homepage",
-			name: "Open Homepage",
+			name: "Open homepage",
 			callback: this.openHomepage,
-		});
-		
+		});		
 		if(this.app.workspace.activeLeaf == null) {
 			//only do on startup, not plugin activation
 			this.app.workspace.onLayoutReady(this.openHomepage);
@@ -28,7 +32,7 @@ export default class Homepage extends Plugin {
 	}
 	
 	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+		this.settings = Object.assign({}, DEFAULT, await this.loadData());
 	}
 
 	async saveSettings() {
@@ -36,37 +40,68 @@ export default class Homepage extends Plugin {
 	}
 	
 	openHomepage = async (): Promise<void> => {
-		await this.app.workspace.openLinkText(this.settings.defaultNote, "", false, { active: true });
+		if(this.workspacesMode()) {
+			if(!(this.settings.defaultNote in this.workspacePlugin?.instance.workspaces)) {
+				new Notice(`Cannot find the workspace "${this.settings.defaultNote}" to use as the homepage.`);
+				return;
+			}
+			
+			this.workspacePlugin.instance.loadWorkspace(this.settings.defaultNote);
+		}
+		else {
+			await this.app.workspace.openLinkText(this.settings.defaultNote, "", false, { active: true });
+		}
 	};
+	
+	workspacesMode(): boolean {
+		return this.workspacePlugin?.enabled && this.settings.workspaceEnabled;
+	}
 }
 
 class HomepageSettingTab extends PluginSettingTab {
 	plugin: Homepage;
+	settings: HomepageSettings;
 
 	constructor(app: App, plugin: Homepage) {
 		super(app, plugin);
 		this.plugin = plugin;
+		this.settings = plugin.settings;
 	}
 
 	display(): void {
 		let {containerEl} = this;
-		let currentValue = this.plugin.settings.defaultNote;
-		let defaultValue = DEFAULT_SETTINGS.defaultNote;
-
 		containerEl.empty();
-
+		
 		new Setting(containerEl)
 			.setName("Open on startup")
 			.setDesc(
+				this.plugin.workspacesMode() ?
+				"The name of the workspace to open on startup." :
 				"The name of the note to open on startup. If it doesn't exist, a new note will be created."
 			)
 			.addText(text => text
 				.setPlaceholder("Home")
-				.setValue(defaultValue == currentValue ? "" : currentValue)
+				.setValue(DEFAULT.defaultNote == this.settings.defaultNote ? "" : this.settings.defaultNote)
 				.onChange(async (value) => {
-					this.plugin.settings.defaultNote = value == "" ? defaultValue : value;
+					this.settings.defaultNote = value || DEFAULT.defaultNote;
 					await this.plugin.saveSettings();
-				})
+				}
+			)
+		);
+		
+		if(this.plugin.workspacePlugin?.enabled) {
+			new Setting(containerEl)
+				.setName("Use workspaces")
+				.setDesc("Open a workspace, instead of a note, as the homepage.")
+				.addToggle(toggle => toggle
+					.setValue(this.settings.workspaceEnabled)
+					.onChange(async value => {
+						this.settings.workspaceEnabled = value;
+						await this.plugin.saveSettings();
+						this.display(); //update open on startup's text
+					}
+				)
 			);
+		}
 	}
 }
