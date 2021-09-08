fc0e34d0f8974f362341c2da9c49e95562b14adb
mirnovov <anon185441@gmail.com>
2021-09-08 09:00:40 +0000

    Add opening modes

--- main.ts
+++ main.ts
@@ -1,15 +1,23 @@
 import { App, Notice, Plugin, PluginSettingTab, Setting, addIcon } from "obsidian";
 
+enum Mode {
+	ReplaceAll = "Replace all open notes",
+	ReplaceLast = "Replace last note",
+	Retain = "Keep open notes"
+}
+
 interface HomepageSettings {
 	defaultNote: string,
 	workspaceEnabled: boolean,
-	hasRibbonIcon: boolean
+	hasRibbonIcon: boolean,
+	openMode: string
 }
 
 const DEFAULT: HomepageSettings = {
 	defaultNote: "Home",
 	workspaceEnabled: false,
-	hasRibbonIcon: true
+	hasRibbonIcon: true,
+	openMode: "Replace last note"
 }
 
 const ICON: string = `<svg fill="currentColor" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"><path d="M12.484 3.1 9.106.075.276 7.769h2.112v10.253h4.189v.006h4.827v-.006h3.954V7.769h2.339l-2.339-2.095v-4.48l-2.874.04V3.1zM7.577 17.028h2.9v-3.439h-2.9v3.439zm6.781-9.259h-3.671v3.24H7.313v-3.24H3.388v9.253h3.189v-4.433h4.9v4.433h2.881V7.769zm-4.671.222v2.018H8.313V7.991h1.374zM2.946 6.769h12.136l-2.598-2.326-3.387-3.034-6.151 5.36zm11.412-1.99-.874-.783V2.22l.874-.012v2.571z"/></svg>`
@@ -39,7 +47,11 @@ export default class Homepage extends Plugin {
 			//only do on startup, not plugin activation
 			this.app.workspace.onLayoutReady(this.openHomepage);
 		}
-		console.log(`Default note: ${this.settings.defaultNote}`);
+		
+		console.log(
+			`Homepage: ${this.settings.defaultNote}`+ 
+			`(mode: ${this.settings.openMode}, workspaces: ${this.settings.workspaceEnabled})`
+		);
 	}
 	
 	async loadSettings() {
@@ -58,10 +70,22 @@ export default class Homepage extends Plugin {
 			}
 			
 			this.workspacePlugin.instance.loadWorkspace(this.settings.defaultNote);
+			return;
+		}
+		else if (this.settings.openMode != Mode.ReplaceAll) {
+			const isExtant = this.app.workspace.getLeavesOfType("markdown").some(
+				leaf => (leaf.view as any).file.basename == this.settings.defaultNote
+			);
+			
+			if (isExtant) return;
 		}
 		else {
-			await this.app.workspace.openLinkText(this.settings.defaultNote, "", false, { active: true });
+			this.app.workspace.detachLeavesOfType("markdown");
 		}
+		
+		await this.app.workspace.openLinkText(
+			this.settings.defaultNote, "", this.settings.openMode == Mode.Retain, { active: true }
+		);
 	};
 	
 	workspacesMode(): boolean {
@@ -127,5 +151,23 @@ class HomepageSettingTab extends PluginSettingTab {
 			)
 		);
 		ribbonSetting.descEl.createDiv({ text: "Takes effect on startup.", attr: {class: "mod-warning"}});
+		
+		
+		if(!this.plugin.workspacesMode()) {
+			new Setting(containerEl)
+				.setName("Opening mode")
+				.setDesc("Determine how existing notes are affected on startup.")
+				.addDropdown(async dropdown => {
+					for (let key of Object.values(Mode)) {
+						dropdown.addOption(key, key);
+					}
+					dropdown.setValue(this.settings.openMode);
+					dropdown.onChange(async option => { 
+						this.settings.openMode = option; 
+						await this.plugin.saveSettings();
+					});
+				}
+			);
+		}
 	}
 }
