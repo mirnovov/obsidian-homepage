aefc7305de10efd74d7699335b3bb998bc83f68a
mirnovov <anon185441@gmail.com>
2022-01-19 09:15:07 +0000

    Improve build script

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,8 +1,14 @@
 import esbuild from "esbuild";
 import process from "process";
 import fs from "fs";
+import { dirname } from "path";
 
 const prod = (process.argv[2] === "production");
+const outPath = (process.argv[2] != undefined && !prod) ? process.argv[2] : "./out/main.js";
+const outDir = dirname(outPath);
+
+if (!fs.existsSync(outDir)) fs.mkdirSync(outDir);
+fs.copyFile("manifest.json", `${outDir}/manifest.json`, e => { if (e) throw e });
 
 esbuild.build({
 	entryPoints: ["src/main.ts"],
@@ -14,9 +20,5 @@ esbuild.build({
 	logLevel: "info",
 	sourcemap: prod ? false : "inline",
 	treeShaking: true,
-	outfile: (process.argv[2] != undefined && !prod) ? process.argv[2] : "out/main.js"
+	outfile: outPath
 }).catch(e => { throw e });
-
-if (prod) {
-	fs.copyFile("manifest.json", "./out/manifest.json", e => { if (e) throw e });
-}
