2f245bbab2911fe8a7fd6a3512c7c573d33c0fe4
novov <anon185441@gmail.com>
2022-10-14 01:29:43 +0000

    Update icon to match new default theme

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { MarkdownView, Notice, Platform, Plugin, WorkspaceLeaf, addIcon, moment 
 import { DEFAULT, Mode, View, HomepageSettings, HomepageSettingTab  } from "./settings";
 import { getDailynotesAutorun, getNewTabPagePlugin, getWorkspacePlugin, getDataviewPlugin, trimFile, upgradeSettings } from "./utils";
 
-const ICON: string = `<svg fill="currentColor" viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linejoin:round;stroke-miterlimit:2"><path d="M12.484 3.1 9.106.075.276 7.769h2.112v10.253h4.189v.006h4.827v-.006h3.954V7.769h2.339l-2.339-2.095v-4.48l-2.874.04V3.1zM7.577 17.028h2.9v-3.439h-2.9v3.439zm6.781-9.259h-3.671v3.24H7.313v-3.24H3.388v9.253h3.189v-4.433h4.9v4.433h2.881V7.769zm-4.671.222v2.018H8.313V7.991h1.374zM2.946 6.769h12.136l-2.598-2.326-3.387-3.034-6.151 5.36zm11.412-1.99-.874-.783V2.22l.874-.012v2.571z"/></svg>`
+const ICON: string = `<svg viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" xml:space="preserve" style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:round;stroke-linejoin:round;stroke-miterlimit:1.5"><path d="M10.025 21H6v-7H3v-1.5L12 3l9 9.5V14h-3v7h-4v-7h-3.975v7Z" style="fill:none;stroke:currentColor;stroke-width:2px"/></svg>`
 
 export default class Homepage extends Plugin {
 	settings: HomepageSettings;
@@ -51,7 +51,7 @@ export default class Homepage extends Plugin {
 		);
 	}
 	
-	async onunload() {
+	async onunload(): Promise<void> {
 		let ntp = getNewTabPagePlugin(this.app);
 		if (!ntp) return;
 		ntp.checkForNewTab = ntp._checkForNewTab;
